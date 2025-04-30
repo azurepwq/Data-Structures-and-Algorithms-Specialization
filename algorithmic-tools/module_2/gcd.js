703c4321853a6d8aac93fcb35d072d5cf7401831
azurepwq <azurepwq@gmail.com>
2025-04-30 09:14:43 +0800

    add gcd

---
+++ algorithmic-tools/module_2/gcd.js
@@ -0,0 +1,28 @@
+// by Alexander Nikolskiy
+
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    terminal: false
+});
+
+process.stdin.setEncoding('utf8');
+rl.on('line', readLine);
+
+function readLine(line) {
+    if (line !== "\n") {
+        const a = parseInt(line.toString().split(' ')[0], 10);
+        const b = parseInt(line.toString().split(' ')[1], 10);
+
+        console.log(gcd(a, b));
+        process.exit();
+    }
+}
+
+function gcd(a, b) {
+    // write your code here
+    [a, b] = [Math.abs(a), Math.abs(b)];
+    return b === 0 ? a : gcd(b, a % b);
+}
+
+module.exports = gcd;
